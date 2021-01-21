6af9e9c9287f758fe9f86b96b3a3c38b2d5c412e
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-21 15:04:06 +0600

    3-Random-number-random-number-between-1-to-6 added

---
+++ 3-Random-number-random-number-between-1-to-6.js
@@ -0,0 +1,10 @@
+// num = 2.336;
+// var result1 = Math.floor(num);
+// var result2 = Math.ceil(num);
+// var result3 = Math.round(num);
+
+for (var i = 0; i < 10; i++) {
+    var randomNum = Math.random() * 6; // 6 means jei koyta random number cai
+    var output = Math.round(randomNum);
+    console.log(output);
+}
